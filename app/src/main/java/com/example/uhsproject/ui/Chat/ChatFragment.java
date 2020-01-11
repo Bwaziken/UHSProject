879d91e939a9032ff6d838ad9ed65246aadfc347
Bwaziken <56092532+Bwaziken@users.noreply.github.com>
2020-01-11 15:04:33 -0500

    chat can send multiple messages now

--- app/src/main/java/com/example/uhsproject/ui/Chat/ChatFragment.java
+++ app/src/main/java/com/example/uhsproject/ui/Chat/ChatFragment.java
@@ -17,23 +17,40 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.uhsproject.R;
 
 public class ChatFragment extends Fragment {
-    String message;
-    static EditText messageBox;
-    static Button send;
-    TextView top;
+    String message,messageBefore,messageBeforeBefore,message3,message4;
+    EditText messageBox;
+    Button send;
+    TextView top,one,two,three,four;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_chats, container, false);
+
         top = (TextView) view.findViewById(R.id.chatTop);//sent messages
+        one = (TextView) view.findViewById(R.id.chatpt);//sent messages
+        two = (TextView) view.findViewById(R.id.chatpt2);//sent messages
+        three = (TextView) view.findViewById(R.id.chatpt3);
+        four = (TextView) view.findViewById(R.id.chatpt4);
+
         messageBox = (EditText) view.findViewById(R.id.messageBox);//chat fragment stuff
         send = (Button) view.findViewById(R.id.send);
+
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                
+                message4=message3;
+                message3=messageBeforeBefore;
+                messageBeforeBefore=messageBefore;
+                messageBefore=message;
+
                 message = messageBox.getText().toString();
                 top.setText(message);
+                one.setText(messageBefore);
+                two.setText(messageBeforeBefore);
+                three.setText(message3);
+                four.setText(message4);
             }
         });
         return view;
