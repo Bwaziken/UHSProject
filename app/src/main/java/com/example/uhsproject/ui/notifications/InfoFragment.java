5f2b8b19fc689cb505a477775b2cf142743ced60
op8mnl <zheng.felix@outlook.com>
2020-01-11 22:01:02 -0500

    Database fixes

--- app/src/main/java/com/example/uhsproject/ui/notifications/InfoFragment.java
+++ app/src/main/java/com/example/uhsproject/ui/notifications/InfoFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -14,11 +16,27 @@ import com.example.uhsproject.R;
 
 public class InfoFragment extends Fragment {
     DatabaseHelper mDatabaseHelper;
-
+    Button registerBtn;
+    EditText nameField;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        View view =  inflater.inflate(R.layout.fragment_info, container, false);
+        registerBtn = (Button) view.findViewById(R.id.registerBtn);
+        nameField = (EditText) view.findViewById(R.id.nameField);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String newEntry = nameField.getText().toString();
+                if (nameField.length() != 0){
+                    AddData(newEntry);
+                    nameField.setText("");
+                }else {
+                    toastMessage("Yo enter something wasteyute");
+                }
+            }
+        });
+        return view;
     }
 
     public void AddData(String newEntry){
@@ -31,15 +49,7 @@ public class InfoFragment extends Fragment {
         }
     }
 
-    public void onClick(){
-        String newEntry = editText.getText().toStirng();
-        if (editText.length() != 0){
-            AddData(newEntry);
-            editText.setText("");
-        }else {
-            toastMessage("Yo enter something wasteyute");
-        }
-    }
+
     private void toastMessage(String message){
         Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
     }
