afd753a163040b1f41ffafc2efe074103ce7a18d
op8mnl <zheng.felix@outlook.com>
2020-01-12 18:41:04 -0500

    Yuh Yuh Yuh

--- app/src/main/java/com/example/uhsproject/LoginScreen.java
+++ app/src/main/java/com/example/uhsproject/LoginScreen.java
@@ -1,19 +1,23 @@
 package com.example.uhsproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginScreen extends AppCompatActivity {
     Button signin;
+    DatabaseHelper mDatabaseHelper;
+    Button registerBtn;
+    EditText nameField, descField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
-
         signin=(Button) findViewById(R.id.signin);
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +26,31 @@ public class LoginScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+            mDatabaseHelper = new DatabaseHelper(this);
+            registerBtn = findViewById(R.id.registerBtn);
+            nameField = findViewById(R.id.nameField);
+            descField = findViewById(R.id.descField);
+            registerBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    if (nameField.length() != 0 && descField.length() != 0 ){
+                        boolean isInserted = mDatabaseHelper.addData(nameField.getText().toString(),descField.getText().toString());
+                        if (isInserted){
+                            toastMessage("You good my nigga");
+                        }else{
+                            toastMessage("Ay no good my man");
+                        }
+                        nameField.setText("");
+                        descField.setText("");
+                    }else {
+                        toastMessage("Yo enter something wasteyute");
+                    }
+                }
+            });
+        }
+        private void toastMessage(String message){
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
     }
-}
+
--- app/src/main/java/com/example/uhsproject/ui/notifications/InfoFragment.java
+++ app/src/main/java/com/example/uhsproject/ui/notifications/InfoFragment.java
@@ -4,51 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.uhsproject.DatabaseHelper;
 import com.example.uhsproject.R;
 
 public class InfoFragment extends Fragment {
-
-    DatabaseHelper mDatabaseHelper;
-    Button registerBtn;
-    EditText nameField, descField;
-
-
-    @Nullable
-    @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_info, container, false);
-        mDatabaseHelper = new DatabaseHelper(getActivity().getApplicationContext());
-        registerBtn = view.findViewById(R.id.registerBtn);
-        nameField = view.findViewById(R.id.nameField);
-        descField = view.findViewById(R.id.descField);
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                if (nameField.length() != 0 && descField.length() != 0 ){
-                    boolean isInserted = mDatabaseHelper.addData(nameField.getText().toString(),descField.getText().toString());
-                    if (isInserted){
-                        toastMessage("You good my nigga");
-                    }else{
-                        toastMessage("Ay no good my man");
-                    }
-                    nameField.setText("");
-                    descField.setText("");
-                }else {
-                    toastMessage("Yo enter something wasteyute");
-                }
-            }
-        });
-    return view;
-    }
-    private void toastMessage(String message){
-        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+        View view = inflater.inflate(R.layout.fragment_info, container, false);
+
+
+        return view;
     }
 }
