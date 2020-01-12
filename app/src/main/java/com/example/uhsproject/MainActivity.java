95d834a42943198f394cdcc11e7c863d4226eae5
op8mnl <zheng.felix@outlook.com>
2020-01-12 17:23:32 -0500

    Yuh Yuh Yuh

--- app/src/main/java/com/example/uhsproject/MainActivity.java
+++ app/src/main/java/com/example/uhsproject/MainActivity.java
@@ -4,9 +4,10 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +21,9 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    DatabaseHelper mDatabaseHelper;
+    Button registerBtn;
+    EditText nameField, descField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +43,35 @@ public class MainActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new HomeFragment()).commit();
         }
+        mDatabaseHelper = new DatabaseHelper (this);
+        registerBtn = findViewById(R.id.registerBtn);
+        nameField = findViewById(R.id.nameField);
+        descField = findViewById(R.id.descField);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if (nameField.length() != 0 && descField.length() != 0 ){
+                    boolean isInserted = mDatabaseHelper.addData(nameField.getText().toString(),descField.getText().toString());
+                    if (isInserted){
+                        toastMessage("You good my nigga");
+                    }else{
+                        toastMessage("Ay no good my man");
+                    }
+                    nameField.setText("");
+                    descField.setText("");
+                }else {
+                    toastMessage("Yo enter something wasteyute");
+                }
+            }
+        });
     }
 
+
+
+
+    private void toastMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
