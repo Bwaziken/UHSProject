3442e9802241e8e1aa7238e58cdeaff99340e2d6
TrashCanMan6969 <59186278+TrashCanMan6969@users.noreply.github.com>
2020-01-12 20:08:38 -0500

    List update

--- app/src/main/java/com/example/uhsproject/TutorFragment.java
+++ app/src/main/java/com/example/uhsproject/TutorFragment.java
@@ -4,16 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class TutorFragment extends Fragment {
+
+
+public class TutorFragment extends Fragment implements View.OnClickListener{
+Button book;
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+
         View view = inflater.inflate(R.layout.fragment_tutorinfo, container, false);
 
+        book=(Button)view.findViewById(R.id.book);
+        book.setOnClickListener(this);
         return view;
     }
 
 
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getContext(),"Appointment Booked",Toast.LENGTH_LONG).show();
+    }
 }
